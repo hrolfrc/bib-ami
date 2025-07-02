d37371278b3f2d21a132aaf7a5c72f78b08d7cad
rolf <hrolfrc@gmail.com>
2025-07-02 17:18:09 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,85 +1,26 @@
-#! /usr/bin/env python
-"""The calfcv package setup file.
+from setuptools import setup, find_packages
 
-Directions
-
-1.  Update the code and documentation
-
-2.  Test the template by running test_template.py
-
-3.  Update the documentation, including the jupyter notebooks
-
-3.1 Convert the notebook content to rst and integrate it into quick_start.rst and user_guide.rst
-
-    jupyter nbconvert quick_start.ipynb --to markdown --output README.md
-    jupyter nbconvert quick_start.ipynb --to rst --output quick_start.rst
-    jupyter nbconvert user_guide.ipynb --to rst --output user_guide.rst
-
-3.2 Check the documentation by reading it from _build/html
-
-4.  Remove the old distribution: rm -r dist
-
-5. cd to calfcv and build the new dist folder:
-python setup.py sdist bdist_wheel
-
-Note that setup is deprecated and a replacement method is needed.
-
-6. Upload to pip: twine upload dist/*
-
-
-"""
-
-import codecs
-
-from setuptools import find_packages, setup
-
-# noinspection PyProtectedMember
-from calfcv import _version
-
-DISTNAME = 'calfcv'
-DESCRIPTION = 'Coarse approximation linear function with cross validation'
-with codecs.open('README.rst', encoding='utf-8-sig') as f:
-    LONG_DESCRIPTION = f.read()
-MAINTAINER = 'Carlson Research, LLC'
-MAINTAINER_EMAIL = 'hrolfrc@gmail.com'
-URL = 'https://github.com/hrolfrc/calfcv'
-LICENSE = 'new BSD'
-DOWNLOAD_URL = 'https://github.com/hrolfrc/calfcv'
-VERSION = _version.__version__
-INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn']
-CLASSIFIERS = ['Intended Audience :: Science/Research',
-               'Intended Audience :: Developers',
-               'Topic :: Scientific/Engineering :: Artificial Intelligence',
-               'Development Status :: 3 - Alpha',
-               'License :: OSI Approved',
-               'Topic :: Scientific/Engineering',
-               'Operating System :: OS Independent',
-               'Programming Language :: Python :: 3']
-
-EXTRAS_REQUIRE = {
-    'tests': [
-        'pytest',
-        'pytest-cov'],
-    'docs': [
-        'sphinx',
-        'sphinx-gallery',
-        'sphinx_rtd_theme',
-        'numpydoc',
-        'matplotlib'
-    ]
-}
-
-setup(name=DISTNAME,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      description=DESCRIPTION,
-      license=LICENSE,
-      url=URL,
-      version=VERSION,
-      download_url=DOWNLOAD_URL,
-      long_description=LONG_DESCRIPTION,
-      zip_safe=False,  # the package can run out of an .egg file
-      classifiers=CLASSIFIERS,
-      packages=find_packages(),
-      install_requires=INSTALL_REQUIRES,
-      extras_require=EXTRAS_REQUIRE)
+setup(
+    name="bib-clean",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "bib-clean = bib_clean.bib_clean:main",
+        ],
+    },
+    author="Rolf Carlson",
+    author_email="hrolfrc@gmail.com",
+    description="A tool to merge and clean BibTeX files.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hrolfrc/bib-clean",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
