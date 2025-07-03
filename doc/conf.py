7bee184432c56c3504d7f1549623162c079e99cb
rolf <hrolfrc@gmail.com>
2025-07-02 21:24:59 -0600

    Updated badging url for codecov.

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,7 @@
 project = 'bib-ami'
 copyright = '2025, Rolf Carlson'
 author = 'Rolf Carlson'
-release = '0.2.7'
+release = '0.2.8'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
