3d2eb0e68b661a2eb93e571fd0092a82648d5bfa
rolf <hrolfrc@gmail.com>
2025-07-05 14:23:38 -0600

    Fixed a bug in the CircleCI config.yml. Changed setup.py to pyproject.toml as a critical file to check.

--- doc/conf.py
+++ doc/conf.py
@@ -4,7 +4,7 @@ project = 'bib-ami'
 # noinspection PyShadowingBuiltins
 copyright = '2025, Rolf Carlson'
 author = 'Rolf Carlson'
-release = '0.10.7'
+release = '0.10.8'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
