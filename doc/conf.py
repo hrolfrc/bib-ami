46f91a4fa9dfd723d6cf5259935f0f682f03f0d1
rolf <hrolfrc@gmail.com>
2025-07-15 17:03:20 -0600

    chore: release 0.15.2

--- doc/conf.py
+++ doc/conf.py
@@ -4,7 +4,7 @@ project = 'bib-ami'
 # noinspection PyShadowingBuiltins
 copyright = '2025, Rolf Carlson'
 author = 'Rolf Carlson'
-release = '0.15.1'
+release = '0.15.2'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
